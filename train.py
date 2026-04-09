2ffda7ebe07b9fde9cae4459658f6c5af7cd1bb7
Jishnu Satwik <jishnusatwikkcp@gmail.com>
2026-04-09 20:10:21 +0530

    ran few experiments and played with parameters

--- train.py
+++ train.py
@@ -1,10 +1,16 @@
 import sys
 import os
+import json
+
+with open("params.json", "r") as f:
+    params = json.load(f)
+
+lr = params["lr"]
 
 with open(os.path.join("dataset", "processed_data.txt"), "r") as f:
     data = f.read()
 
-model = f"Model trained on data: {data}"
+model = f"Model trained on data: {data} with lr={lr}"
 
 with open("model.txt", "w") as f:
     f.write(model)
